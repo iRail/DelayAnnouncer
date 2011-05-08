1afa22ce62821988701b63c72925a82f6614c790
Tim Besard <tim.besard@gmail.com>
2011-05-08 15:01:45 +0200

    Forgot arrival module.

---
+++ lib/WWW/IRail/API2/Arrival.pm
@@ -0,0 +1,84 @@
+################################################################################
+# Configuration
+#
+
+# Package definition
+package WWW::IRail::API2::Arrival;
+
+# Packages
+use Moose;
+use Log::Log4perl qw(:easy);
+
+# Write nicely
+use strict;
+use warnings;
+
+
+################################################################################
+# Attributes
+#
+
+=pod
+
+=head1 ATTRIBUTES
+
+=cut
+
+has 'origin' => (
+	is		=> 'ro',
+	isa		=> 'Str',
+	required	=> 1
+);
+
+has 'time' => (
+	is		=> 'ro',
+	isa		=> 'Int',
+	required	=> 1
+
+);
+
+has 'vehicle' => (
+	is		=> 'ro',
+	isa		=> 'Maybe[Str]'
+);
+
+has 'platform' => (
+	is		=> 'ro',
+	isa		=> 'Maybe[Int]'
+);
+
+has 'delay' => (
+	is		=> 'ro',
+	isa		=> 'Int',
+	default		=> 0
+);
+
+
+################################################################################
+# Methods
+#
+
+=pod
+
+=head1 METHODS
+
+=cut
+
+
+42;
+
+__END__
+
+=pod
+
+=head1 COPYRIGHT
+
+Copyright 2011 The iRail development team as listed in the AUTHORS file.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
